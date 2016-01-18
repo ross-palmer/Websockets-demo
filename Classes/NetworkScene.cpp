3362ecf4795993f9e4ffef28c626c3758ed0a9ec
ross-palmer <ross.palmer@gmail.com>
2016-01-18 14:16:48 +0000

    Updated NetworkScene to work with new server

--- Classes/NetworkScene.cpp
+++ Classes/NetworkScene.cpp
@@ -2,8 +2,7 @@
 
 USING_NS_CC;
 
-std::string const NetworkScene::JSON_MESSAGE("{\"type\": \"join\", \"pid\" : \"Android client\"}");
-
+std::string const NetworkScene::JSON_MESSAGE("{\"type\": \"join\"}");
 
 Scene* NetworkScene::createScene()
 {
@@ -30,25 +29,66 @@ bool NetworkScene::init()
 	
 	
 	// Create a button
-	auto button = ui::Button::create("Connect_Button.png",
-		"Connect_Button(Click).png");
+	auto button = ui::Button::create("Join_Button.png",
+		"Join_Button(Click).png");
 	button->setPosition(Vec2(200, 200));
-
-	button->addTouchEventListener(CC_CALLBACK_1(NetworkScene::handleTouch, this));
-	this->addChild(button);
 	
-	mSocket = SWebSocket::create("ws://localhost:8080/wstest");
+	button->addTouchEventListener([&](Ref* sender, ui::Widget::TouchEventType type){
+		switch (type)
+		{
+		case ui::Widget::TouchEventType::BEGAN:
+			break;
+		case ui::Widget::TouchEventType::ENDED:
+			handleTouch(this);
+			break;
+		default:
+			break;
+		}
+	});
+
+	button->setName("JoinButton");
+	this->addChild(button);	
+	
+	addEvents();	// Add scene touch handler
+
+	mSocket = SWebSocket::create("ws://localhost:8081/test");
 
 	mSocket->onMessageReceived = std::bind(&NetworkScene::onReceive, this, std::placeholders::_1);
 	
 	mSocket->connect();
 
+	
 	return true;
 }
+
+void NetworkScene::addEvents()
+{
+	//Create a "one by one" touch event listener (processes one touch at a time)
+	auto listener1 = EventListenerTouchOneByOne::create();
+	// When "swallow touches" is true, then returning 'true' from the onTouchBegan method will "swallow" the touch event, preventing other listeners from using it.
+	listener1->setSwallowTouches(true);
+
+	// Example of using a lambda expression to implement onTouchBegan event callback function
+	listener1->onTouchBegan = [this](Touch* touch, Event* event){
+		this->m_pos = touch->getLocation();
+		updateState();
+		return true;
+	};
+
+	cocos2d::Director::getInstance()->getEventDispatcher()->addEventListenerWithFixedPriority(listener1, 30);
+}
+
 
 void NetworkScene::handleTouch(Ref *pSender)
 {
 	mSocket->send(JSON_MESSAGE);
+	// Check if the join button is still visible.
+	auto buttonNode = this->getChildByName("JoinButton");	
+	if (buttonNode != nullptr)
+	{
+		// Remove the join button
+		this->removeChildByName("JoinButton");
+	}
 }
 
 void NetworkScene::onReceive(std::string message)
@@ -70,13 +110,41 @@ void NetworkScene::onReceive(std::string message)
 	}
 }
 
+void NetworkScene::updateState()
+{	
+	CCLOG("Touch x,y pos = : %d, %d", this->m_pos.x, this->m_pos.y);	
+	std::string touchX = std::to_string(this->m_pos.x);
+	std::string touchY = std::to_string(this->m_pos.y);	
+	std::string jsonMessage = "{\"type\": \"updateState\", \"data\" : {\"x\":" + touchX + ", \"y\":" + touchY + "}}";	
+	mSocket->send(jsonMessage);
+}
+
+
 void NetworkScene::parseJSONMessage(std::string message)
 {	
 	m_document.Parse<0>(message.c_str());
 	auto it1 = m_document.MemberonBegin();
-	// Get value of "data" 
-	m_data = it1->value.GetString();
-	// Get value of "type" 
+	// Hold a reference to the "data" field which may be a number of a string (depending on the "type" field)
+	auto & data = it1->value;
 	it1++;
-	m_type = it1->value.GetString();
+	// Get the "type" field which is always a string
+	m_type = it1->value.GetString();	
+	if (m_type == "gameState")
+	{
+		// get a number: 0 = WAITING_FOR_PLAYERS, 1 = STARTING_GAME
+		int num = data.GetUint();		
+		m_data = std::to_string(num);
+	}
+	else if (m_type == "error")
+	{
+		m_data = data.GetString();
+	}
+	else if (m_type == "updateState")
+	{
+		// Retrieve the touch position as two separate x,y values 	
+		double x = data["x"].GetDouble();
+		double y = data["y"].GetDouble();
+		m_data = "x: " + std::to_string(x) + ", y: " + std::to_string(y);
+	}
+	
 }
--- Classes/NetworkScene.h
+++ Classes/NetworkScene.h
@@ -11,6 +11,7 @@ using namespace std::placeholders;
 class NetworkScene : public cocos2d::Layer
 {
 private:
+	cocos2d::Vec2 m_pos;	// Touch event location.
 	static std::string const JSON_MESSAGE;
 	rapidjson::Document m_document;	// A container for a JSON document
 	std::string m_data;	
@@ -21,13 +22,18 @@ private:
 
 	SWebSocket* mSocket;
 	void parseJSONMessage(std::string message);
+	//void parseUpdateMessage(std::string message);
 
 public:
 	static cocos2d::Scene* createScene();
 	virtual bool init();
 
+	void addEvents();
+
 	void handleTouch(Ref *pSender);
 	void onReceive(std::string message);
+
+	void updateState();
 
 	CREATE_FUNC(NetworkScene);
 };
